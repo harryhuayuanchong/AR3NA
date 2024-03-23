17fd2923774678fc5f6492b38edf3bbe433f3c9e
brianhsieh01 <chyuanxie@gmail.com>
2024-03-23 18:56:18 +0800

    feat: install corsheaders feat: modify Event model

--- backend/main/apps/ticketing/models/models.py
+++ backend/main/apps/ticketing/models/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Event(models.Model):
     name = models.CharField(max_length=255)
-    image = models.CharField(max_length=255)
+    image = models.CharField(max_length=255, null=True)
     genre = models.CharField(max_length=255)
     date = models.DateField()
-    time = models.TimeField()
+    time = models.TimeField(null=True)
     information = models.TextField()
     location = models.CharField(max_length=255)
     is_created = models.BooleanField(default=False)
--- backend/main/settings/base.py
+++ backend/main/settings/base.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'main.apps.ticketing.apps.TicketingConfig',
+    'corsheaders',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -50,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'main.urls'
