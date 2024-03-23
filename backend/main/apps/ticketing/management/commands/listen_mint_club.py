861ea71608d552c124ad8ea0cd6e4ffe5af09869
brianhsieh01 <chyuanxie@gmail.com>
2024-03-23 09:47:56 +0800

    feat: add event listener for ticket creation feat: add Event, TicketSBT, TicketBC model

---
+++ backend/main/apps/ticketing/management/commands/listen_mint_club.py
@@ -0,0 +1,63 @@
+from django.core.management.base import BaseCommand
+from web3 import Web3
+import requests
+import json
+import time
+import os
+from main.apps.ticketing.models import TicketBC
+
+class Command(BaseCommand):
+    help = 'Continuously listen for MultiTokenCreated events and update or create TicketBC entries accordingly.'
+
+    def handle(self, *args, **options):
+        web3 = Web3(Web3.HTTPProvider('https://eth-sepolia-public.unifra.io'))
+
+        mintclub_abi = self.load_abi('MintClub.json')
+        mintclub_contract = web3.eth.contract(address='0x8dce343A86Aa950d539eeE0e166AFfd0Ef515C0c', abi=mintclub_abi)
+
+        event_filter = mintclub_contract.events.MultiTokenCreated.create_filter(fromBlock='latest')
+        print("Listening for MultiTokenCreated events...")
+
+        while True:
+            events = event_filter.get_new_entries()
+            print('events', events)
+            if events:
+                for event in events:
+                    tx_hash = event['transactionHash'].hex()
+                    tx_receipt = web3.eth.get_transaction(tx_hash)
+                    creator_address = tx_receipt['from']
+                    uri = event['args']['uri']
+                    external_url, image = self.fetch_ipfs_content(uri)
+
+                    TicketBC.objects.update_or_create(
+                        contract_address=event['args']['token'],
+                        defaults={
+                            'contract_owner_address': creator_address,
+                            'base_uri': uri,
+                            'ticket_name': event['args']['name'],
+                            'ticket_symbol': event['args']['symbol'],
+                            'mint_club_site': external_url,
+                            'image': image
+                        }
+                    )
+                    print(f"Updated or created TicketBC for {event['args']['name']}")
+            time.sleep(10)
+
+    def fetch_ipfs_content(self, uri):
+        external_url, image = '', ''
+        if uri.startswith("ipfs://"):
+            ipfs_hash = uri.split("ipfs://")[1]
+            ipfs_url = f"https://ipfs.io/ipfs/{ipfs_hash}"
+            try:
+                response = requests.get(ipfs_url)
+                ipfs_content = response.json()
+                external_url = ipfs_content.get('external_url', '')
+                image = ipfs_content.get('image', '').replace('ipfs://', 'https://ipfs.io/ipfs/')
+            except Exception as e:
+                print(f"Failed to fetch or parse IPFS content for URI {uri}: {e}")
+        return external_url, image
+
+    def load_abi(self, file_name):
+        path = os.path.join(os.path.dirname(__file__), '../../abi', file_name)
+        with open(path, 'r') as file:
+            return json.load(file)
--- backend/main/apps/ticketing/models/models.py
+++ backend/main/apps/ticketing/models/models.py
@@ -1,1 +1,41 @@
 from django.db import models
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    date = models.DateField()
+    time = models.TimeField()
+    information = models.TextField()
+    location = models.CharField(max_length=255)
+    is_created = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.name}"
+    
+class TicketSBT(models.Model):
+    contract_name = models.CharField(max_length=255)
+    contract_address = models.CharField(max_length=255)
+    contract_owner_address = models.CharField(max_length=255)
+    base_uri = models.CharField(max_length=255)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True)
+    ticket_id = models.IntegerField()
+    ticket_name = models.CharField(max_length=255)
+    ticket_fee = models.DecimalField(max_digits=10, decimal_places=2)
+    ticket_max_supply = models.IntegerField()
+
+    def __str__(self):
+        return f"Ticket(SBT) - {self.contract_name} - {self.ticket_name}"
+    
+class TicketBC(models.Model):
+    contract_address = models.CharField(max_length=255)
+    contract_owner_address = models.CharField(max_length=255)
+    base_uri = models.CharField(max_length=255)
+    ticket_name = models.CharField(max_length=255)
+    ticket_symbol = models.CharField(max_length=255)
+    mint_club_site= models.CharField(max_length=255)
+    image = models.CharField(max_length=255)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'''Ticket(BC) - {self.ticket_name}({self.ticket_symbol})'''
